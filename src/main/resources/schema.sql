3942c542f1f468f3a8308bbe06fbf6b61bf07753
Thomas Moran <thomas@optimizme.com>
2020-05-14 17:51:11 +0100

    Update schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -2,9 +2,8 @@ CREATE TABLE users
 (
   id serial PRIMARY KEY,
   active boolean NOT NULL,
-  user_name UNIQUE character varying(50) NOT NULL,
+  user_name character varying(50) NOT NULL,
   password character varying(50) NOT NULL,
   roles character varying(50) NOT null,
-  created_on TIMESTAMP NOT NULL,
-  CONSTRAINT users_pkey PRIMARY KEY (id)
+  created_on TIMESTAMP NOT NULL
 );
